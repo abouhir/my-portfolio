aa4015df5903aaa68f57fde3e5bbde3aed1738ec
cbouhir <abouhircontact@gmail.com>
2023-01-12 23:27:42 +0100

    fixe deploy app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
-
-import './App.css';
-import Menu from './components/Menu';
-import Home from './components/Home';
-import { Route, Routes } from 'react-router-dom';
-import Competence from './components/Competence';
-import Formation from './components/Formation';
-import Experience from './components/Experience';
-import Contact from './components/Contact';
-import Annonce from './components/Annonce';
-import { useEffect } from 'react';
+import "./App.css";
+import Menu from "./components/Menu";
+import Home from "./components/Home";
+import { Route, Routes } from "react-router-dom";
+import Competence from "./components/Competence";
+import Formation from "./components/Formation";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Annonce from "./components/Annonce";
+import { useEffect } from "react";
 
 function App() {
-  
   useEffect(() => {
-    
- sessionStorage.setItem('isHidden','0') ;
- 
+    sessionStorage.setItem("isHidden", "0");
   });
 
- 
-    
- 
   return (
-
     <Routes>
-      <Route  element={<Menu />}> 
-      <Route path='/' element={<Annonce />} >
-        <Route index element={<Home />} />
+      <Route element={<Menu />}>
+        <Route path="/" element={<Annonce />}>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/competences" element={<Competence />} />
+        <Route path="/formations" element={<Formation />} />
+        <Route path="/experiences" element={<Experience />} />
+        <Route path="/contacts" element={<Contact />} />
       </Route>
-        <Route path='/competences' element={<Competence />} />
-        <Route path='/formations' element={<Formation />} />
-        <Route path='/experiences' element={<Experience />} />
-        <Route path='/contacts' element={<Contact />} />
-       
-      </Route>
-     
-  
     </Routes>
-  
-
   );
 }
 
